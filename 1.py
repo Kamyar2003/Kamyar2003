1cddfc2cbe3f5c81d7660c390118635c97d51b78
Kamyar <103222462+Kamyar2003@users.noreply.github.com>
2025-07-14 20:44:12 +0330

    1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,7 @@
-def get_median_word(self, text: str) -> str:
+def get_median_word(text: str) -> str:
     half_length = len(text) // 2
     return text[half_length] if len(text) % 2 != 0 else text[half_length - 1 : half_length + 1]
+
+
+print(get_median_word("Python")) # th
+print(get_median_word("Hello")) # l
