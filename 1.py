2bcf0e6353abff158c8f1be2bb858a0baa7ce925
Kamyar <103222462+Kamyar2003@users.noreply.github.com>
2025-07-14 20:41:13 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,3 @@
-def f(x: bool) -> bool:
-  return not x
-
-print(f(True))
+def get_median_word(self, text: str) -> str:
+    half_length = len(text) // 2
+    return text[half_length] if len(text) % 2 != 0 else text[half_length - 1 : half_length + 1]
